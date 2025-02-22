f6dfc2f3487fa1e64a69560e21f36d53db0859f6
Zahra <zahra.mohamed.n7@gmail.com>
2025-02-22 18:51:21 +0800

    Chore: Set up backend project using start.spring.io

---
+++ src/test/java/com/example/BookingSystemBackend/BookingSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookingSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
